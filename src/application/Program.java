3039cf307936bbb1a9fb5e4ad9eb5f043dde4774
Denner Marchi <denner883@hotmail.com>
2019-10-02 19:12:17 -0300

    Possible moves of a piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,7 +15,7 @@ public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        
+
         while (true) {
             try {
                 UI.clearScreen();
